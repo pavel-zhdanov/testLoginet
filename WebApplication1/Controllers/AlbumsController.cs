06b3488715712d3266987010f633be8617d4d909
zhdanov <p.zhdanov1993@gmail.com>
2018-12-18 23:36:17 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/AlbumsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public AlbumsController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        // GET api/albums
+        [HttpGet]
+        public async Task<ActionResult<List<Album>>> GetAllAlbums()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "http://jsonplaceholder.typicode.com/albums/");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                List<Album> albums = await response.Content.ReadAsAsync<List<Album>>();
+                return Ok(albums);
+            }
+
+            return NotFound();
+        }
+
+        // GET api/albums/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Album>> GetAlbumsById(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            $"http://jsonplaceholder.typicode.com/albums/{id}");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Album album = await response.Content.ReadAsAsync<Album>();
+                return Ok(album);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public UsersController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUser()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "http://jsonplaceholder.typicode.com/users/");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                List<User> users = await response.Content.ReadAsAsync<List<User>>();
+                return Ok(users);
+            }
+
+            return NotFound();
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            $"http://jsonplaceholder.typicode.com/users/{id}");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                User user = await response.Content.ReadAsAsync<User>();
+                return Ok(user);
+            }
+
+            return NotFound();
+        }
+
+        // GET api/users/5/albums
+        [HttpGet("{id}/albums")]
+        public async Task<ActionResult<List<Album>>> GetUserAlbumById(int id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            $"http://jsonplaceholder.typicode.com/users/{id}/albums");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                List<Album> albums = await response.Content.ReadAsAsync<List<Album>>();
+                return Ok(albums);
+            }
+
+            return NotFound();
+        }
+    }
+}
